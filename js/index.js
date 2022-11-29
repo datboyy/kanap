103b739a58a1f0de69fab38086185811dc052068
Thomas LHTS <thomas.lhts.95@icloud.com>
2022-11-29 15:07:47 +0100

    :feat: products filled with products infos

--- js/index.js
+++ js/index.js
@@ -3,44 +3,46 @@
 // Homepage's products list
 //
 //
-
+// API URL
+const host = 'http://localhost:3000'
+const productsListURI = host + '/api/products/'
 //
 // Selectors & elements
 let itemsSel = 'section.items'
 let itemsEl = document.querySelector(itemsSel)
-
-// API URL
-const host = 'http://localhost:3000'
-const productsListURI = host + '/api/products/'
-
 //
 // Fetch homepage products list
 fetch(productsListURI)
   .then((res) => res.json())
   .then((data) => {
     data.forEach((item) => {
+      //
+      // Container a element
       let aEl = document.createElement('a')
           aEl.setAttribute('href', 'product.html?id=' + item._id)
-
+      //
+      // Article element
       let articleEl = document.createElement('article')
-
+      //
+      // Product illustration image element
       let imgEl = document.createElement('img')
           imgEl.setAttribute('src', item.imageUrl)
           imgEl.setAttribute('alt', item.altTxt)
-
+      //
+      // Product title element
       let h3El = document.createElement('h3')
           h3El.classList.add('productName')
           h3El.innerText = item.name
-
+      //
+      // Product description element
       let pEl = document.createElement('p')
           pEl.classList.add('productDescription')
           pEl.innerText = item.description
-
+      //
+      // Append elements to the DOM
       articleEl.append(imgEl, h3El, pEl)
       aEl.append(articleEl)
-
       itemsEl.insertAdjacentElement('beforeend', aEl)
-
     })
 })
 // EOF
---
+++ js/product.js
@@ -0,0 +1,68 @@
+//
+//
+// Products themselves
+//
+//
+// API URI
+const url = new URL(window.location);
+const id = url.searchParams.get("id");
+const host = 'http://localhost:3000'
+const productsListURI = host + '/api/products/' + id
+//
+// Selectors
+let itemSel = 'section.item'
+let itemImgSel = '.item__img'
+let itemTitleSel = '#title'
+let itemPriceSel = '#price'
+let itemDescSel = '#description'
+let itemColorsSel = '#colors'
+let addToCartBtn = '#addToCart'
+//
+// Elements
+let titleEl = document.querySelector('title')                                   // Page title element
+let itemEl = document.querySelector(itemSel)                                    // Item container element
+let itemImgEl = document.querySelector(itemImgSel)                              // Item image container element
+let itemTitleEl = document.querySelector(itemTitleSel)                          // Item title element
+let itemPriceEl = document.querySelector(itemPriceSel)                          // Item price element
+let itemDescEl = document.querySelector(itemDescSel)                            // Item description element
+let itemColorsEl = document.querySelector(itemColorsSel)                        // Item colors options container element
+let addToCartBtnEl = document.querySelector(addToCartBtn)                       // Item add to cart button element
+//
+// Fetch product data
+fetch(productsListURI)
+  .then((res) => res.json())
+  .then((data) => {
+    //
+    // Page title
+    titleEl.innerText = titleEl.innerText + ' - ' + data.name
+    //
+    // Product image
+    let imgEl = document.createElement('img')
+        imgEl.setAttribute('src', data.imageUrl)
+    itemImgEl.insertAdjacentElement('afterbegin', imgEl)
+    //
+    // Product title
+    itemTitleEl.innerText = data.name
+    //
+    // Product price
+    itemPriceEl.innerText = data.price
+    //
+    // Product description
+    itemDescEl.innerText = data.description
+    //
+    // Product colors
+    data.colors.forEach((color) => {
+      // Create an option element
+      let colorsOptionEl = document.createElement('option')
+          colorsOptionEl.setAttribute('value', color)
+          colorsOptionEl.innerText = color
+      // Append option element to the DOM
+      itemColorsEl.insertAdjacentElement('beforeend', colorsOptionEl)
+    })
+  })
+  //
+  // Add-to-cart button event
+  addToCartBtn.addEventListener('click', (ev) => {
+    // @TODO
+  })
+// EOF
